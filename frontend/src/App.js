ea40d0b2849ac67b36d1d496c67d167fdbe9c92a
jacklusy <jackalloussi23@gmail.com>
2023-02-26 16:27:47 +0300

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,41 +21,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
-import './assets/css/libs.min.css'; 
+import './assets/css/libs.min.css';
 import './assets/css/socialv.css?v=4.0.0';
 import './assets/vendor/@fortawesome/fontawesome-free/css/all.min.css';
 import './assets/vendor/remixicon/fonts/remixicon.css';
 import './assets/vendor/vanillajs-datepicker/dist/css/datepicker.min.css';
 import './assets/vendor/line-awesome/dist/font-awesome-line-awesome/css/all.min.css';
 import './assets/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css';
-import Login from "./components/main/login/Login";
-import Register from "./components/main/register/Register";
 //import Login from "./components/main/Login";
 
 
 
 const App = () => {
   return (
-        <>
+    <>
       <Router>
 
-          <Routes>
-           <Route exact path="/" element={<Login />} />
-           <Route exact path="/register" element={<Register />} />
-            <Route exact path="/home" element={<Index />} />
+        <Routes>
+          <Route exact path="/" element={<Index />} />
 
-            <Route exact path="/Profile" element={<Profile />} />
-            <Route exact path="/EditProfile" element={<EditProfile />}  />
-            <Route exact path="/ProfileForum" element={<ProfileForum />}  />
+          <Route exact path="/Profile" element={<Profile />} />
+          <Route exact path="/EditProfile" element={<EditProfile />} />
+          <Route exact path="/ProfileForum" element={<ProfileForum />} />
 
-            <Route exact path="/Group" element={<Group />}  />
-            <Route exact path="/GroupDetails" element={<GroupDetails />}  />
+          <Route exact path="/Group" element={<Group />} />
+          <Route exact path="/GroupDetails" element={<GroupDetails />} />
 
-            <Route exact path="/Chat" element={<Chat />}  />
-            <Route exact path="/Email" element={<Email />}  />
-          </Routes>
+          <Route exact path="/Chat" element={<Chat />} />
+          <Route exact path="/Email" element={<Email />} />
+        </Routes>
       </Router>
-      </>
+    </>
   )
 }
 
