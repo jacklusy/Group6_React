6708e4d8002ef345b57b32310d4a11d27c4a8986
unknown <salwayehyaabuafash@gmail.com>
2023-02-26 22:58:42 +0200

    salwa

--- frontend/src/components/main/Login.js
+++ frontend/src/components/main/Login.js
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
+//import loginImg from './logo-full.png'
+
 // import 'react-toastify/dist/ReactToastify.css';
 
 
@@ -62,30 +64,13 @@ const Login = () => {
             
             <div className="container p-0">
               <div className="row no-gutters">
-                <div className="col-md-6 text-center pt-5">
-                  <div className="sign-in-detail text-white">
-                    <a className="sign-in-logo mb-5" href="#"><img src="../assets/images/logo-full.png" className="img-fluid" alt="logo" /></a>
-                    <div className="sign-slider overflow-hidden ">
-                      <ul className="swiper-wrapper list-inline m-0 p-0 ">
-                        <li className="swiper-slide">
-                          <img src="../assets/images/login/1.png" className="img-fluid mb-4" alt="logo" />
-                          <h4 className="mb-1 text-white">Find new friends</h4>
-                          <p>It is a long established fact that a reader will be distracted by the readable content.</p>
-                        </li>
-                        <li className="swiper-slide">
-                          <img src="../assets/images/login/2.png" className="img-fluid mb-4" alt="logo" /> 
-                          <h4 className="mb-1 text-white">Connect with the world</h4>
-                          <p>It is a long established fact that a reader will be distracted by the readable content.</p>
-                        </li>
-                        <li className="swiper-slide">
-                          <img src="../assets/images/login/3.png" className="img-fluid mb-4" alt="logo" />
-                          <h4 className="mb-1 text-white">Create new events</h4>
-                          <p>It is a long established fact that a reader will be distracted by the readable content.</p>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
+               
+
+
+
+
+
+                
                 <div className="col-md-6 bg-white pt-5 pt-5 pb-lg-0 pb-5">
                   <div className="sign-in-from">
                     <h1 className="mb-0">Sign in</h1>
@@ -94,8 +79,7 @@ const Login = () => {
                       <div className="form-group">
 
 
-                        <label className="form-label" htmlFor="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control mb-0" id="exampleInputEmail1" placeholder="Enter email" 
+                        <input type="email" className="form-control mb-0" id="exampleInputEmail1" placeholder="Email" 
                         value={email}  onChange={e=>setEmail(e.target.value)} name="email"  
                         />
                            {error && email.length === 0 &&
@@ -107,7 +91,7 @@ const Login = () => {
 
 
                       <div className="form-group">
-                        <label className="form-label" htmlFor="exampleInputPassword1">Password</label>
+                        {/* <label className="form-label" htmlFor="exampleInputPassword1">Password</label> */}
                         <input type="password" className="form-control mb-0" id="exampleInputPassword1" placeholder="Password"
                         value={password} onChange={e=>passwordUpdate(e.target.value)}  name="password" 
                         />
@@ -135,6 +119,33 @@ const Login = () => {
 
 
                     
+                  </div>
+                </div>
+                <div className="col-md-6 text-center pt-5">
+                  <div className="sign-in-detail text-white">
+                  <div className="loginImage">
+                  {/* <img src={loginImg}  alt="login" className="img-fluid"/> */}
+                </div>
+                    {/* <a className="sign-in-logo mb-5" ><img src="../assets/images/logo-full.png" className="img-fluid" alt="logo" /></a> */}
+                    <div className="sign-slider overflow-hidden ">
+                      <ul className="swiper-wrapper list-inline m-0 p-0 ">
+                        <li className="swiper-slide">
+                          <img src="../assets/images/login/1.png" className="img-fluid mb-4" alt="logo" />
+                          <h4 className="mb-1 text-white">Find new friends</h4>
+                          <p>It is a long established fact that a reader will be distracted by the readable content.</p>
+                        </li>
+                        <li className="swiper-slide">
+                          <img src="../assets/images/login/2.png" className="img-fluid mb-4" alt="logo" /> 
+                          <h4 className="mb-1 text-white">Connect with the world</h4>
+                          <p>It is a long established fact that a reader will be distracted by the readable content.</p>
+                        </li>
+                        <li className="swiper-slide">
+                          <img src="../assets/images/login/3.png" className="img-fluid mb-4" alt="logo" />
+                          <h4 className="mb-1 text-white">Create new events</h4>
+                          <p>It is a long established fact that a reader will be distracted by the readable content.</p>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
