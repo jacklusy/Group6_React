cddc25d6fae03135be63be0fc321da3db8fe80c4
unknown <salwayehyaabuafash@gmail.com>
2023-02-26 21:33:55 +0200

    salwa

--- frontend/src/components/main/Login.js
+++ frontend/src/components/main/Login.js
@@ -1,6 +1,60 @@
-import React from 'react'
+import React , { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Login = () => {
+  const navigate = useNavigate();
+
+
+  const [email, setEmail] = useState('');
+  const [password, passwordUpdate] = useState('');
+  const [users, setUsers] = useState([]);
+  const [error , setError] = useState(false);
+
+ 
+      useEffect(()=>{
+        getUser()
+    },[]);
+
+
+    const ProceedLogin = (e) => {
+      e.preventDefault(); 
+      for(const user of users){
+  
+        if(email.length===0 || password.length===0 ){
+           setError(true)
+        }else{
+          if(user.email === email && user.password === password && Object.entries(email).length >0 && Object.entries(password).length >0){
+             
+             localStorage.setItem('Id' , JSON.stringify(user.id))
+             localStorage.setItem('Email' , JSON.stringify(user.email))
+             localStorage.setItem('Password' , JSON.stringify(user.password))
+              console.log(`Welcome User`)
+              toast.success('Welcome User ❤️')
+              navigate('/home')
+              return;
+             } }  
+          }
+              console.log("Wrong email or password");
+             
+              toast.error('Wrong email or password');
+               
+          
+          
+     
+     
+  }
+  
+      const getUser = ()=> {
+        axios.get('http://localhost/ReactBreef/Reactphp/log_reg.php').then(function(response){
+            setUsers(response.data)
+        })
+    }
+
     return (
   
         <div className="wrapper">
@@ -35,33 +89,52 @@ const Login = () => {
                 <div className="col-md-6 bg-white pt-5 pt-5 pb-lg-0 pb-5">
                   <div className="sign-in-from">
                     <h1 className="mb-0">Sign in</h1>
-                    <p>Enter your email address and password to access admin panel.</p>
+
                     <form className="mt-4">
                       <div className="form-group">
+
+
                         <label className="form-label" htmlFor="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control mb-0" id="exampleInputEmail1" placeholder="Enter email" />
+                        <input type="email" className="form-control mb-0" id="exampleInputEmail1" placeholder="Enter email" 
+                        value={email}  onChange={e=>setEmail(e.target.value)} name="email"  
+                        />
+                           {error && email.length === 0 &&
+                        <label style ={{color:'red'}}>Email is required</label>}
+                        {email.length > 0 && !(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z]{2,4}$/.test(email)) && (
+                        <label style ={{color:'red'}}>This is not a valid email</label> 
+                        )}
                       </div>
+
+
                       <div className="form-group">
                         <label className="form-label" htmlFor="exampleInputPassword1">Password</label>
-                        <a href="#" className="float-end">Forgot password?</a>
-                        <input type="password" className="form-control mb-0" id="exampleInputPassword1" placeholder="Password" />
+                        <input type="password" className="form-control mb-0" id="exampleInputPassword1" placeholder="Password"
+                        value={password} onChange={e=>passwordUpdate(e.target.value)}  name="password" 
+                        />
+                         {error&&password.length===0?
+                     <label style ={{color:'red'}}>Password is required</label>:""}
                       </div>
+
+
                       <div className="d-inline-block w-100">
                         <div className="form-check d-inline-block mt-2 pt-1">
                           <input type="checkbox" className="form-check-input" id="customCheck11" />
                           <label className="form-check-label" htmlFor="customCheck11">Remember Me</label>
                         </div>
-                        <button type="submit" className="btn btn-primary float-end">Sign in</button>
+                        <button onClick={ProceedLogin} type="submit" className="btn btn-primary float-end">Log in</button>
                       </div>
                       <div className="sign-info">
-                        <span className="dark-color d-inline-block line-height-2">Don't have an account? <a href="sign-up.html">Sign up</a></span>
-                        <ul className="iq-social-media">
+                        <span className="dark-color d-inline-block line-height-2">Don't have an account? <Link to={'/register'}> Sign up</Link></span>
+                        {/* <ul className="iq-social-media">
                           <li><a href="#"><i className="ri-facebook-box-line" /></a></li>
                           <li><a href="#"><i className="ri-twitter-line" /></a></li>
                           <li><a href="#"><i className="ri-instagram-line" /></a></li>
-                        </ul>
+                        </ul> */}
                       </div>
                     </form>
+
+
+                    
                   </div>
                 </div>
               </div>
